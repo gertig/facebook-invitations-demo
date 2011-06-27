b109cd13b144aa89bfb45dcc06d72d411b80c94e
AndrewGertig <andrew.gertig@gmail.com>
2011-06-26 23:41:59 -0400

    Changed app id and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,17 +1,18 @@
 #APP_ID, APP_SECRET
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook , '109969585764747', '595d25190451d00d0ff89829a9bbc7a3', :iframe => true, 
+  provider :facebook , '138781652866563', '5825f4b91f3ecce46efebca3df717aa2', :iframe => true, 
       :scope => 'email, user_about_me,user_activities,user_birthday,user_education_history,
                       user_events,user_groups,user_hometown,user_interests,user_likes, user_location,
                       user_religion_politics,
                       user_status,user_work_history,
                       publish_stream, offline_access,
-                      friends_about_me,
-                      friends_activities,friends_birthday,friends_education_history,friends_events,
-                      friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,
-                      friends_photo_video_tags,friends_photos,friends_relationships,
-                      friends_relationship_details,friends_religion_politics,friends_status,
-                      friends_work_history' 
+                      friends_about_me'
+                      
+                      # friends_activities,friends_birthday,friends_education_history,friends_events,
+                      # friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,
+                      # friends_photo_video_tags,friends_photos,friends_relationships,
+                      # friends_relationship_details,friends_religion_politics,friends_status,
+                      # friends_work_history
                       
                       # user_relationships, user_relationship_details,
                       # user_photo_video_tags,user_photos,
